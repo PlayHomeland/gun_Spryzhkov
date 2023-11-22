0da5b6a78fff54bb9634fdd428689156aabde100
PlayHomeland <spryzkov@gmail.com>
2023-11-22 12:14:46 +0300

    Added score counter, added multiple types of targets: classic, jumping and static, added multiple types of balls: classic (MAGENTA), bomb (CYAN), 'piercing through' (GREEN), divisible (YELLOW)

--- gun.py
+++ gun.py
@@ -16,14 +16,14 @@ CYAN = 0x00FFCC
 BLACK = (0, 0, 0)
 WHITE = 0xFFFFFF
 GREY = 0x7D7D7D
-GAME_COLORS = [RED, BLUE, YELLOW, GREEN, MAGENTA, CYAN]
+GAME_COLORS = [YELLOW, GREEN, MAGENTA, CYAN]
 
 WIDTH = 800
 HEIGHT = 600
 
 
 class Ball:
-    def __init__(self, screen: pygame.Surface, x=40, y=450):
+    def __init__(self, screen: pygame.Surface, color, vx = 0, vy = 0, x=40, y=450):
         """ Конструктор класса ball
 
         Args:
@@ -34,9 +34,9 @@ class Ball:
         self.x = x
         self.y = y
         self.r = 10
-        self.vx = 0
-        self.vy = 0
-        self.color = choice(GAME_COLORS)
+        self.vx = vx
+        self.vy = vy
+        self.color = color
         self.live = 200
 
     def move(self):
@@ -73,7 +73,6 @@ class Ball:
             Возвращает True в случае столкновения мяча и цели. В противном случае возвращает False.
         """
         if (self.x-obj.x)**2 + (self.y-obj.y)**2<=(self.r+obj.r)**2:
-            self.live = 0
             return True
         else:
             return False
@@ -102,7 +101,7 @@ class Gun:
         """
         global balls, bullet
         bullet += 1
-        new_ball = Ball(self.screen)
+        new_ball = Ball(self.screen, choice(GAME_COLORS))
         new_ball.r += 5
         self.an = math.atan2((event.pos[1]-new_ball.y), (event.pos[0]-new_ball.x))
         new_ball.vx = self.f2_power * math.cos(self.an)
@@ -134,15 +133,14 @@ class Gun:
         if self.f2_on:
             if self.f2_power < 100:
                 self.f2_power += 1
-            self.color = RED
+            self.color = YELLOW
         else:
             self.color = GREY
 
 
-class Target:
+class Target_classic:
     # self.points = 0
     # self.live = 1
-    # FIXME: don't work!!! How to call this functions when object is created?
     # self.new_target()
 
     def __init__(self, screen: pygame.Surface):
@@ -151,15 +149,15 @@ class Target:
         self.screen = screen
         self.r = randint(20, 50)
         self.x = randint(600, 780)
-        self.y = randint(300, 540-self.r)
+        self.y = randint(self.r+1, 540-self.r)
         self.color = RED
         self.live = 1
-        self.points = 0
         self.vy = randint(0,10)
 
-    def hit(self, points=1):
+    def hit(self, point=1):
         """Попадание шарика в цель."""
-        self.points += points
+        global points
+        points += point
 
     def move(self):
         """Переместить мяч по прошествии единицы времени.
@@ -169,8 +167,83 @@ class Target:
         и стен по краям окна (размер окна 800х600).
         """
         self.y -= self.vy
-        if self.y >=500 or self.y<= 20:
+        if self.y >= 540 - self.r or self.y <= self.r:
             self.vy = -self.vy
+
+    def draw(self):
+        pygame.draw.circle(
+            self.screen,
+            self.color,
+            (self.x, self.y),
+            self.r
+        )
+
+class Target_jumping:
+    # self.points = 0
+    # self.live = 1
+    # self.new_target()
+
+    def __init__(self, screen: pygame.Surface):
+        """
+        """
+        self.screen = screen
+        self.r = randint(20, 50)
+        self.x = randint(600, 780)
+        self.y = randint(self.r, 540-self.r)
+        self.color = BLUE
+        self.period = randint(30, 60)
+        self.time = self.period
+        self.live = 1
+
+    def hit(self, point=1):
+        """Попадание шарика в цель."""
+        global points
+        points += point
+
+    def move(self):
+        """Переместить мяч по прошествии единицы времени.
+
+        Метод описывает перемещение мяча за один кадр перерисовки. То есть, обновляет значения
+        self.x и self.y с учетом скоростей self.vx и self.vy, силы гравитации, действующей на мяч,
+        и стен по краям окна (размер окна 800х600).
+        """
+        self.time -= 1
+        if self.time == 0:
+            self.y = randint(self.r, 540 - self.r)
+            self.time = self.period
+
+    def draw(self):
+        pygame.draw.circle(
+            self.screen,
+            self.color,
+            (self.x, self.y),
+            self.r
+        )
+
+class Target_static:
+    # self.points = 0
+    # self.live = 1
+    # self.new_target()
+
+    def __init__(self, screen: pygame.Surface):
+        """
+        """
+        self.screen = screen
+        self.r = randint(20, 50)
+        self.x = randint(600, 780)
+        self.y = randint(self.r, 540-self.r)
+        self.color = BLACK
+        self.live = 1
+
+    def hit(self, point=1):
+        """Попадание шарика в цель."""
+        global points
+        points += point
+
+    def move(self):
+        """
+        """
+        pass
 
     def draw(self):
         pygame.draw.circle(
@@ -186,15 +259,24 @@ screen = pygame.display.set_mode((WIDTH, HEIGHT))
 bullet = 0
 balls = []
 targets = []
+points = 0
 
 clock = pygame.time.Clock()
 gun = Gun(screen)
-for i in range(2):
-    targets.append(Target(screen))
+targets.append(Target_classic(screen))
+targets.append(Target_jumping(screen))
+targets.append(Target_static(screen))
+targets.append(Target_static(screen))
+targets.append(Target_static(screen))
 finished = False
+
+
 
 while not finished:
     screen.fill(WHITE)
+    fonts = [pygame.font.SysFont('times new roman', 20)]
+    text1 = fonts[0].render("Your score is: "+str(points), True, BLACK)
+    screen.blit(text1, (0, 0))
     gun.draw()
     for target in targets:
         target.draw()
@@ -215,14 +297,43 @@ while not finished:
 
     for target in targets:
         target.move()
-        for b in balls:
-            b.move()
+    for b in balls:
+        b.move()
+        for target in targets:
             if b.hittest(target) and target.live:
                 target.live = 0
                 target.hit()
+                if b.color == MAGENTA:
+                    b.live = 0
+                elif b.color == CYAN:
+                    b.live = 0
+                    b.r = 100
+                    for t in targets:
+                        if b.hittest(t) and target.live:
+                            target.live = 0
+                            target.hit()
+                            t.__init__(screen)
+                            if isinstance(t, Target_classic) and len(targets)<=10:
+                                targets.append(Target_jumping(screen))
+                                targets[-1].draw()
+                            if isinstance(t, Target_jumping) and len(targets)<=10:
+                                targets.append(Target_classic(screen))
+                                targets[-1].draw()
+
+                elif b.color == YELLOW:
+                    b.live = 0
+                    for i in range(3):
+                        balls.append(Ball(screen, MAGENTA, randint(-10, 10), randint(-5, 5), b.x, b.y))
                 target.__init__(screen)
-            if b.live<=0:
-                balls.remove(b)
-        gun.power_up()
+                if isinstance(target, Target_classic) and len(targets)<=10:
+                    targets.append(Target_jumping(screen))
+                    targets[-1].draw()
+                if isinstance(target, Target_jumping) and len(targets)<=10:
+                    targets.append(Target_classic(screen))
+                    targets[-1].draw()
+
+        if b.live <= 0:
+            balls.remove(b)
+    gun.power_up()
 
 pygame.quit()
